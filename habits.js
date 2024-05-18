08ad15b8ef0f74b0c13e04434660b4340a14704b
David L <dliu19244@gmail.com>
2024-05-18 23:45:42 +1000

    Habit Graphs are now properly interactive

--- habits.js
+++ habits.js
@@ -1,5 +1,5 @@
 // establishing global variables containing csv data
-var age, anxiety, celebrity, fomo;
+var age, anxiety, celebrity, fomo, anxietyChart, celebrityChart,fomoChart;
 
 Plotly.d3.csv("data/smhabits.csv", habit_data => {
     //Unpack all coloumns
@@ -8,7 +8,16 @@ Plotly.d3.csv("data/smhabits.csv", habit_data => {
     celebrity = unpack(habit_data, 'Celebrity').map(Number);
     fomo = unpack(habit_data, 'FOMO').map(Number);
     
-    updateGraphs();
+    //Initialise Graphs
+    //Chart 1: Celebrity Followers
+    celebrityChart = generateRadialGraph(celebrity, age,"#habits_celeb" , "Celebrity Followers");
+    celebrityChart.render();
+    //Chart 2: Prevalance of anxiety disorders
+    anxietyChart = generateRadialGraph(anxiety, age, "#habits_anxiety", "Prevalance of anxiety disorders");
+    anxietyChart.render();
+    // //Chart 3: Fear of Missing Out
+    fomoChart = generateRadialGraph(fomo, age, "#habits_fomo", "Fear of Missing Out");
+    fomoChart.render()
 });
 
 function roundToOne(num) {
@@ -54,90 +63,100 @@ function updateGraphs(ageBracket = -1){
         console.log(age_values);
 
         //Chart 1: Celebrity Followers
-        generateRadialGraph(celebrity_values, age_values,"#habits_celeb" , "Celebrity Followers");
+        celebrityChart.updateOptions({
+                series: celebrity_values,
+                labels: age_values
+            }, 
+            false, true);
+
         //Chart 2: Prevalance of anxiety disorders
-        // generateRadialGraph(anxiety_values, age_values, "#habits_anxiety", "Prevalance of anxiety disorders");
+        anxietyChart.updateOptions({
+                series: anxiety_values,
+                labels: age_values
+            }, 
+            false, true);
+
         // //Chart 3: Fear of Missing Out
-        // generateRadialGraph(fomo_values, age_values, "#habits_fomo", "Fear of Missing Out");
+        fomoChart.updateOptions({
+                series: fomo_values,
+                labels: age_values
+            }, 
+            false, true);
+
 }
 
 function generateRadialGraph(values, ages, graphid, title){
     // Values = the percentages displayed on the radial graph
+    
     // graph id  = The id tag on the div to be turned into a graph
+    // Title = The title of the graph to be displayed
 
     /* Below is adapted from https://apexcharts.com/javascript-chart-demos/radialbar-charts/custom-angle-circle/ */
     
+       
     var options = {
         series: values,
+        labels: ages,
         chart: {
             height: 390,
             type: 'radialBar',
-            events: {
-                dataPointSelection: function(event, chartContext, config) {
-                  console.log(config);
-                  console.log(event);
-                  console.log(chartContext);
-                }
-            }
         },
         plotOptions: {
             radialBar: {
-            offsetY: 0,
-            startAngle: 0,
-            endAngle: 270,
-            hollow: {
-                margin: 5,
-                size: '30%',
-                background: 'transparent',
-                image: undefined,
-            },
-            dataLabels: {
-                name: {
-                show: false,
-                },
-                value: {
-                show: false,
-                }
-            },
-            barLabels: {
-                enabled: true,
-                useSeriesColors: true,
-                margin: 8,
-                fontSize: '16px',
-                formatter: function(seriesName, opts) {
-                    return seriesName + ":  " + roundToOne(opts.w.globals.series[opts.seriesIndex]) + "%"
-                }
-            }
-            },
-            title: {
-                text: title,
-                align: 'left',
-                margin: 10,
-                offsetX: 0,
                 offsetY: 0,
-                floating: false,
-                style: {
-                fontSize:  '14px',
-                fontWeight:  'bold',
-                fontFamily:  'Roboto',
-                color:  '#263238'
+                startAngle: 0,
+                endAngle: 270,
+                hollow: {
+                    margin: 5,
+                    size: '30%',
+                    background: 'transparent',
+                    image: undefined,
                 },
-            },        
-            colors: ['#1ab7ea', '#0084ff', '#39539E', '#0077B5'],
-            labels: [ages],
-            responsive: [{
-                breakpoint: 480,
-                options: {
-                    legend: {
-                        show: false
+                dataLabels: {
+                    name: {
+                        show: true,
+                    },
+                    value: {
+                        show: true,
                     }
-                }
-            }]
-        }
+                },
+                barLabels: {
+                    enabled: true,
+                    useSeriesColors: true,
+                    margin: 8,
+                    fontSize: '16px',
+                    formatter: function(seriesName, opts) {
+                        return seriesName + ":  " + roundToOne(opts.w.globals.series[opts.seriesIndex]) +"%"
+                    },
+                },
+            }
+        },
+        title: {
+            text: title,
+            align: 'left',
+            margin: 10,
+            offsetX: 0,
+            offsetY: 0,
+            floating: false,
+            style: {
+            fontSize:  '14px',
+            fontWeight:  'bold',
+            fontFamily:  'Roboto',
+            color:  '#263238'
+            },
+        },
+        colors: ['#1ab7ea', '#0084ff', '#39539E', '#0077B5'],
+        responsive: [{
+            breakpoint: 480,
+            options: {
+            legend: {
+                show: false
+            }
+            }
+        }]
     };
 
-    let chart = new ApexCharts(document.querySelector(graphid), options);
-    chart.render();
+    return new ApexCharts(document.querySelector(graphid), options);
 }
 
 // Retrieving an array contianing the buttons used to alter the graph data
@@ -148,6 +167,6 @@ console.log(buttonArray);
 buttonArray.forEach(button =>{
     button.addEventListener("click", function(event) {
         let ageBracket = parseInt(button.getAttribute("value"));
-        // updateGraphs(ageBracket);
+        updateGraphs(ageBracket);
     });
 });
